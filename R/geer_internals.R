754a22ab8b2a70bb4a81d70aa750e6b2da82d3e2
Anestis Touloumis <A.Touloumis@brighton.ac.uk>
2024-11-21 22:49:57 +0000

    241121

--- R/geer_internals.R
+++ R/geer_internals.R
@@ -24,11 +24,6 @@ compute_quasi_loglikelihood <- function(object) {
 compute_criteria <- function(object, cov_type, digits) {
   quasi_loglikelihood <- compute_quasi_loglikelihood(object)
   if (as.character(object$call[1]) == "geewa") {
-    quasi_loglikelihood_independence <-
-      compute_quasi_loglikelihood(
-        update(object,
-               correlation_structure = "independence")
-      )
     sc_wc_stats <-
       get_gee_criteria_sc_cw(object$y,
                              object$id,
@@ -39,11 +34,9 @@ compute_criteria <- function(object, cov_type, digits) {
                              object$alpha,
                              object$phi)
   } else {
-    quasi_loglikelihood_independence <-
-      compute_quasi_loglikelihood(
-        update(object,
-               or_structure = "independence")
-      )
+    if (length(object$alpha) == 1) {
+      object$alpha <- rep(object$alpha, choose(max(object$repeated), 2))
+    }
     sc_wc_stats <-
       get_gee_criteria_sc_cw_or(object$y,
                                 object$id,
@@ -73,25 +66,23 @@ compute_criteria <- function(object, cov_type, digits) {
                                           object$linear.predictors,
                                           object$phi)
   p <- length(object$coeff)
-  gess <- sc_wc_stats[[1]]/(object$obs_no - p - association_params_no)
-  cwc <- sc_wc_stats[[2]]
+  gessc <- sc_wc_stats[[1]]/(object$obs_no - p - association_params_no)
+  gpc <- sc_wc_stats[[2]]
   qic_u <- 2 * (p - quasi_loglikelihood)
   cic <- sum(diag(independence_naive_covariance_inverse %*% beta_covariance))
   qic <- 2 * (cic - quasi_loglikelihood)
-  qic_hh <- 2 * (cic - quasi_loglikelihood_independence)
   q_matrix <- solve(naive_covariance) %*% beta_covariance
   c1 <- sum(diag(q_matrix)) / p
   c2 <- sum(q_matrix * t(q_matrix)) / p
   rjc <- sqrt(((1 - c1) ^ 2) + ((1 - c2) ^ 2))
   ans <- data.frame(QIC = qic,
-                    QICHH = qic_hh,
                     CIC = cic,
                     RJC = rjc,
                     QICU = qic_u,
-                    GESS = gess,
-                    CWC = cwc,
+                    GESSC = gessc,
+                    GPC = gpc,
                     Parameters = p)
-  ans[, 1:7] <- apply(ans[, 1:7], 2, round, digits = digits)
-  ans[, 8] <- round(ans[, 8], digits = 0)
+  ans[, 1:6] <- apply(ans[, 1:6], 2, round, digits = digits)
+  ans[, 7] <- round(ans[, 7], digits = 0)
   ans
 }
