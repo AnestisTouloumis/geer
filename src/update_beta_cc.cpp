8d01758d394ac1f3b073a9dc0dfefe34384927dd
Anestis Touloumis <A.Touloumis@brighton.ac.uk>
2025-03-06 00:25:59 +0000

    250306: update betas

--- src/update_beta_cc.cpp
+++ src/update_beta_cc.cpp
@@ -24,28 +24,32 @@ arma::vec update_beta_gee_cc(const arma::vec & y_vector,
                              const arma::vec & weights_vector) {
   int params_no = model_matrix.n_cols;
   int sample_size = max(id_vector);
+  int repeated_max = max(repeated_vector);
   arma::mat u_vector = arma::zeros(params_no);
   arma::mat naive_matrix_inverse = arma::zeros(params_no, params_no);
   arma::mat correlation_matrix =
     get_correlation_matrix(correlation_structure,
                            alpha_vector,
-                           max(repeated_vector));
+                           repeated_max);
   arma::vec delta_vector = mueta(link, eta_vector);
   arma::vec s_vector = y_vector - mu_vector;
   for(int i=1; i < sample_size + 1; i++){
     arma::uvec id_vector_i = find(id_vector == i);
-    arma::mat d_matrix_i = arma::diagmat(delta_vector(id_vector_i)) *
-      model_matrix.rows(id_vector_i);
-    arma::mat v_matrix_inverse_i =
-      arma::pinv(get_v_matrix_cc(family,
-                                 mu_vector(id_vector_i),
-                                 repeated_vector(id_vector_i),
-                                 phi,
-                                 correlation_matrix,
-                                 weights_vector(id_vector_i)));
+    arma::mat d_matrix_i =
+      arma::diagmat(delta_vector(id_vector_i)) * model_matrix.rows(id_vector_i);
     arma::mat d_matrix_trans_v_matrix_inverse_i =
-      trans(d_matrix_i) * v_matrix_inverse_i;
-    naive_matrix_inverse += d_matrix_trans_v_matrix_inverse_i * d_matrix_i;
+      trans(
+        solve(
+          get_v_matrix_cc(family,
+                          mu_vector(id_vector_i),
+                          repeated_vector(id_vector_i),
+                          phi,
+                          correlation_matrix,
+                          weights_vector(id_vector_i)),
+          d_matrix_i)
+      );
+    naive_matrix_inverse +=
+      d_matrix_trans_v_matrix_inverse_i * d_matrix_i;
     u_vector +=
       d_matrix_trans_v_matrix_inverse_i * s_vector(id_vector_i);
   }
