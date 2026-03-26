7b2ad929f0be4601b9f3b7bc66b2ab0c2d5f0153
Anestis Touloumis <A.Touloumis@brighton.ac.uk>
2026-03-26 22:58:28 +0000

    260326 geer_internals-validation

--- R/geer_internals-validation.R
+++ R/geer_internals-validation.R
@@ -1,4 +1,3 @@
-## internal validation helpers for geer user-facing functions
 .geer_test_choices <- c(
   "wald", "score", "working-wald", "working-score", "working-lrt"
 )
@@ -15,33 +14,39 @@
   "backward", "forward", "both"
 )
 
+.geer_integer_tol <- sqrt(.Machine$double.eps)
+
 
 `%||%` <- function(x, y) {
   if (is.null(x)) y else x
 }
 
 
-check_geer_object <- function(x, arg = "object") {
-  if (!inherits(x, "geer")) {
-    stop(sprintf("'%s' must be of 'geer' class", arg), call. = FALSE)
+.check_class <- function(x, class_name, arg) {
+  if (!inherits(x, class_name)) {
+    stop(sprintf("'%s' must be of '%s' class", arg, class_name), call. = FALSE)
   }
   x
 }
 
 
+check_geer_object <- function(x, arg = "object") {
+  .check_class(x, class_name = "geer", arg = arg)
+}
+
+
 check_summary_geer_object <- function(x, arg = "x") {
-  if (!inherits(x, "summary.geer")) {
-    stop(sprintf("'%s' must be of 'summary.geer' class", arg), call. = FALSE)
-  }
-  x
+  .check_class(x, class_name = "summary.geer", arg = arg)
 }
 
 
 check_nonnegative_integerish <- function(x, name) {
   check_single_numeric(x, name)
-  if (x < 0 || abs(x - round(x)) > sqrt(.Machine$double.eps)) {
+
+  if (x < 0 || abs(x - round(x)) > .geer_integer_tol) {
     stop(sprintf("'%s' must be a single non-negative integer", name), call. = FALSE)
   }
+
   as.integer(round(x))
 }
 
@@ -58,49 +63,68 @@ check_probability_closed <- function(x, name) {
 }
 
 
-match_test_type <- function(test) {
-  if (missing(test) || is.null(test)) {
-    test <- .geer_test_choices[1L]
+.match_choice_with_default <- function(x,
+                                       choices,
+                                       name,
+                                       default = choices[1L]) {
+  if (missing(x) || is.null(x)) {
+    return(default)
   }
-  check_choice(test, .geer_test_choices, "test")
-  test
+
+  check_choice(x, choices, name)
+  x
+}
+
+
+match_test_type <- function(test) {
+  .match_choice_with_default(
+    x = test,
+    choices = .geer_test_choices,
+    name = "test"
+  )
 }
 
 
 match_cov_type <- function(cov_type) {
-  if (missing(cov_type) || is.null(cov_type)) {
-    cov_type <- .geer_cov_type_choices[1L]
-  }
-  check_choice(cov_type, .geer_cov_type_choices, "cov_type")
-  cov_type
+  .match_choice_with_default(
+    x = cov_type,
+    choices = .geer_cov_type_choices,
+    name = "cov_type"
+  )
 }
 
 
 match_pmethod <- function(pmethod) {
-  if (missing(pmethod) || is.null(pmethod)) {
-    pmethod <- .geer_pmethod_choices[1L]
-  }
-  check_choice(pmethod, .geer_pmethod_choices, "pmethod")
-  pmethod
+  .match_choice_with_default(
+    x = pmethod,
+    choices = .geer_pmethod_choices,
+    name = "pmethod"
+  )
 }
 
 
 match_direction_type <- function(direction) {
-  if (missing(direction) || is.null(direction)) {
-    direction <- .geer_direction_choices[1L]
-  }
-  check_choice(direction, .geer_direction_choices, "direction")
-  direction
+  .match_choice_with_default(
+    x = direction,
+    choices = .geer_direction_choices,
+    name = "direction"
+  )
 }
 
 
 is_working_test <- function(test) {
-  test %in% c("working-wald", "working-score", "working-lrt")
+  identical(test, "working-wald") ||
+    identical(test, "working-score") ||
+    identical(test, "working-lrt")
 }
 
 
-check_working_lrt_allowed <- function(object, test = "working-lrt", arg = "object") {
-  object <- check_geer_object(object, arg)
+check_working_lrt_allowed <- function(object,
+                                      test = "working-lrt",
+                                      arg = "object") {
+  object <- check_geer_object(object, arg = arg)
+  test <- match_test_type(test)
+
   if (identical(test, "working-lrt") &&
       !identical(object$association_structure, "independence")) {
     stop(
@@ -108,6 +132,7 @@ check_working_lrt_allowed <- function(object, test = "working-lrt", arg = "objec
       call. = FALSE
     )
   }
+
   invisible(TRUE)
 }
 
@@ -115,8 +140,9 @@ check_working_lrt_allowed <- function(object, test = "working-lrt", arg = "objec
 check_test_cov_type_compatibility <- function(test, cov_type) {
   test <- match_test_type(test)
   cov_type <- match_cov_type(cov_type)
+
   ## all currently supported test/cov_type combinations are allowed
-  invisible(TRUE)
+  invisible(list(test = test, cov_type = cov_type))
 }
 
 
@@ -126,15 +152,19 @@ normalize_test_options <- function(test,
                                    object = NULL) {
   test <- match_test_type(test)
   cov_type <- match_cov_type(cov_type)
+
   if (is_working_test(test)) {
     pmethod <- match_pmethod(pmethod %||% .geer_pmethod_choices[1L])
   } else {
     pmethod <- NULL
   }
+
   check_test_cov_type_compatibility(test, cov_type)
+
   if (identical(test, "working-lrt") && !is.null(object)) {
     check_working_lrt_allowed(object, test = test)
   }
+
   list(
     test = test,
     cov_type = cov_type,
@@ -146,6 +176,7 @@ normalize_test_options <- function(test,
 validate_step_thresholds <- function(p_enter, p_remove) {
   check_probability_open(p_enter, "p_enter")
   check_probability_open(p_remove, "p_remove")
+
   list(
     p_enter = p_enter,
     p_remove = p_remove
