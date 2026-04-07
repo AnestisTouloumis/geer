415f1956a307305935a37d1f5dd144815c786cf0
Anestis Touloumis <A.Touloumis@brighton.ac.uk>
2026-04-07 17:12:18 +0100

    260407 utils

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,30 +1,25 @@
-#include <RcppArmadillo.h>
-using namespace Rcpp;
+#include "utils.h"
+
 
 //============================ arma to vec =====================================
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
-Rcpp::NumericVector arma2vec(const arma::vec & x) {
-  Rcpp::NumericVector ans = Rcpp::NumericVector(x.begin(), x.end());
-  return(ans);
-  }
-
+Rcpp::NumericVector arma2vec(const arma::vec& x) {
+  return Rcpp::NumericVector(x.begin(), x.end());
+}
 //==============================================================================
 
 
 //============================ vec to arma =====================================
 // [[Rcpp::export]]
-arma::vec vec2arma(const Rcpp::NumericVector & x) {
-  arma::vec ans = Rcpp::as<arma::vec>(x);
-  return(ans);
-  }
+arma::vec vec2arma(const Rcpp::NumericVector& x) {
+  return Rcpp::as<arma::vec>(x);
+}
 //==============================================================================
 
-//============================ subset matrix x[1:y, 1:y] =======================
-// [[Rcpp::depends(RcppArmadillo)]]
+
+//============================ subset matrix x[y, y] ===========================
 arma::mat subset_matrix(const arma::mat& x, const arma::vec& y) {
- // arma::uvec z = arma::conv_to<arma::uvec>::from(arma::vec(y));
-//  return x.submat(z - 1, z - 1);
   arma::uvec z = arma::conv_to<arma::uvec>::from(y);
   z -= 1;
   return x.submat(z, z);
@@ -33,40 +28,29 @@ arma::mat subset_matrix(const arma::mat& x, const arma::vec& y) {
 
 
 //============================ kappa matrix ====================================
-arma::mat kappa_matrix(int dimension) {
-  //arma::mat ans = arma::zeros(dimension * dimension, dimension);
-  //for(int j = 1; j < dimension + 1; j++) {
-  //  ans((j - 1) * dimension + j - 1, j - 1) = 1;
-  //}
-  //return ans;
+arma::mat kappa_matrix(const arma::uword dimension) {
   arma::mat ans(dimension * dimension, dimension, arma::fill::zeros);
+
   for (arma::uword j = 0; j < dimension; ++j) {
     ans(j * dimension + j, j) = 1.0;
   }
   return ans;
 }
-
 //==============================================================================
 
 
 //============================ kronecker(X, I) * kappa =========================
 arma::mat kronecker_left_identity_kappa(const arma::mat& x) {
-  //const int dimension = x.n_rows;
-  //const int dim_sq = dimension * dimension;
-  //arma::mat ans = arma::zeros(dim_sq, dimension);
-  //for(int j = 1; j < dimension + 1; j++) {
-  //  ans.rows((j - 1) * dimension, j * dimension - 1).diag() =
-  //    x.row(j - 1);
-  //}
-  //return ans;
   const arma::uword dimension = x.n_rows;
   arma::mat ans(dimension * dimension, dimension, arma::fill::zeros);
+
   for (arma::uword j = 0; j < dimension; ++j) {
     const arma::uword base = j * dimension;
     for (arma::uword i = 0; i < dimension; ++i) {
       ans(base + i, i) = x(j, i);
     }
   }
+
   return ans;
 }
 //==============================================================================
@@ -74,43 +58,33 @@ arma::mat kronecker_left_identity_kappa(const arma::mat& x) {
 
 //============================ kronecker(I, X) * kappa =========================
 arma::mat kronecker_identity_right_kappa(const arma::mat& x) {
- // const int dimension = x.n_rows;
-  //const int dim_sq = dimension * dimension;
-  //arma::mat ans = arma::zeros(dim_sq, dimension);
-  //for(int  j = 1; j < dimension + 1; j++) {
-  //  ans.submat((j - 1) * dimension, j - 1, j * dimension - 1, j - 1) =
-  //    x.col(j - 1);
-  //}
-  //return(ans);
   const arma::uword dimension = x.n_rows;
   arma::mat ans(dimension * dimension, dimension, arma::fill::zeros);
+
   for (arma::uword j = 0; j < dimension; ++j) {
     ans.submat(j * dimension, j, (j + 1) * dimension - 1, j) = x.col(j);
   }
+
   return ans;
 }
 //==============================================================================
 
 
-//============================ Kappa * X  ======================================
+//============================ Kappa * X =======================================
 arma::mat kappa_right(const arma::mat& x) {
-//  const int dimension = x.n_rows;
-//  const int dim_sq = dimension * dimension;
-//  arma::mat ans = arma::zeros(dim_sq, dimension);
-//  for(int  j = 1; j < dimension + 1; j++) {
-//    ans.row((j - 1) * (dimension + 1)) = x.row(j - 1);
-//  }
-//  return(ans);
-const arma::uword dimension = x.n_rows;
+  const arma::uword dimension = x.n_rows;
   arma::mat ans(dimension * dimension, dimension, arma::fill::zeros);
-  arma::uvec rows = arma::regspace<arma::uvec>(0, dimension - 1) * (dimension + 1);
+
+  const arma::uvec rows =
+    arma::regspace<arma::uvec>(0, dimension - 1) * (dimension + 1);
   ans.rows(rows) = x;
+
   return ans;
 }
 //==============================================================================
 
 
-//============================ kronecker direct sum of the same matrix =========
+//============================ kronecker direct sum of same matrix =============
 arma::mat kronecker_sum_same(const arma::mat& x) {
   const arma::uword dimension = x.n_rows;
   const arma::mat identity_mat = arma::eye(dimension, dimension);
@@ -119,7 +93,7 @@ arma::mat kronecker_sum_same(const arma::mat& x) {
 //==============================================================================
 
 
-//============================ kronecker(x, I)  ================================
+//============================ kronecker(x, I) =================================
 arma::mat kronecker_vector_identity(const arma::vec& x) {
   const arma::uword dimension = x.n_rows;
   return arma::kron(x, arma::eye(dimension, dimension));
@@ -127,67 +101,89 @@ arma::mat kronecker_vector_identity(const arma::vec& x) {
 //==============================================================================
 
 
-//============================ kronecker(x, Y)   ===============================
+//============================ kronecker(x, Y) =================================
 arma::mat kronecker_vector_matrix(const arma::vec& x, const arma::mat& y) {
   return arma::kron(x, y);
 }
 //==============================================================================
 
 
-//============================ X^{-1} y  =======================================
+//============================ X^{-1} y ========================================
 arma::vec solve_chol_or_lu_vec(const arma::mat& X, const arma::vec& y) {
   arma::mat chol_upper;
   if (arma::chol(chol_upper, X)) {
-    arma::vec chol_intermediate = arma::solve(arma::trimatl(chol_upper.t()), y);
-    return arma::solve(arma::trimatu(chol_upper), chol_intermediate);
+    const arma::vec forward =
+      arma::solve(arma::trimatl(chol_upper.t()), y);
+    return arma::solve(arma::trimatu(chol_upper), forward);
   }
+
   arma::mat lu_lower, lu_upper, permutation_matrix;
   arma::lu(lu_lower, lu_upper, permutation_matrix, X);
-  arma::vec permutation_vector = permutation_matrix * y;
-  arma::vec lu_forward = arma::solve(arma::trimatl(lu_lower), permutation_vector, arma::solve_opts::allow_ugly);
-  return arma::solve(arma::trimatu(lu_upper), lu_forward,  arma::solve_opts::allow_ugly);
-}
 
-//=========================== X^{-1} Y  ========================================
+  const arma::vec permuted_rhs = permutation_matrix * y;
+  const arma::vec forward =
+    arma::solve(arma::trimatl(lu_lower), permuted_rhs,
+                arma::solve_opts::allow_ugly);
+
+  return arma::solve(arma::trimatu(lu_upper), forward,
+                     arma::solve_opts::allow_ugly);
+}
+//==============================================================================
+
+
+//============================ X^{-1} Y ========================================
 arma::mat solve_chol_or_lu_mat(const arma::mat& X, const arma::mat& Y) {
   arma::mat chol_upper;
   if (arma::chol(chol_upper, X)) {
-    arma::mat Z = arma::solve(arma::trimatl(chol_upper.t()), Y);
-    return arma::solve(arma::trimatu(chol_upper), Z);
+    const arma::mat forward =
+      arma::solve(arma::trimatl(chol_upper.t()), Y);
+    return arma::solve(arma::trimatu(chol_upper), forward);
   }
+
   arma::mat lu_lower, lu_upper, permutation_matrix;
   arma::lu(lu_lower, lu_upper, permutation_matrix, X);
-  arma::mat permuted_matrix = permutation_matrix * Y;
-  arma::mat Z = arma::solve(arma::trimatl(lu_lower), permuted_matrix, arma::solve_opts::allow_ugly);
-  return arma::solve(arma::trimatu(lu_upper), Z,  arma::solve_opts::allow_ugly);
-}
 
-// ---- lambda blocks
+  const arma::mat permuted_rhs = permutation_matrix * Y;
+  const arma::mat forward =
+    arma::solve(arma::trimatl(lu_lower), permuted_rhs,
+                arma::solve_opts::allow_ugly);
+
+  return arma::solve(arma::trimatu(lu_upper), forward,
+                     arma::solve_opts::allow_ugly);
+}
+//==============================================================================
+
+
+//============================ lambda from matrix blocks =======================
 arma::vec lambda_from_blocks_chol_or_lu(const arma::mat& A,
                                         const arma::mat& lambda_matrix) {
   const arma::uword p = A.n_rows;
   arma::vec lambda_vector(p, arma::fill::zeros);
-
-  arma::mat R;
-  if (arma::chol(R, A)) {
+  arma::mat chol_upper;
+  if (arma::chol(chol_upper, A)) {
     for (arma::uword r = 0; r < p; ++r) {
       const arma::mat block = lambda_matrix.rows(r * p, (r + 1) * p - 1);
-      arma::mat Y = arma::solve(arma::trimatl(R.t()), block);
-      arma::mat X = arma::solve(arma::trimatu(R), Y);
-      lambda_vector[r] = -0.5 * arma::trace(X);
+      const arma::mat forward =
+        arma::solve(arma::trimatl(chol_upper.t()), block);
+      const arma::mat solved =
+        arma::solve(arma::trimatu(chol_upper), forward);
+      lambda_vector[r] = -0.5 * arma::trace(solved);
     }
     return lambda_vector;
   }
-
-  arma::mat L, U, P;
-  arma::lu(L, U, P, A);
-
+  arma::mat lu_lower, lu_upper, permutation_matrix;
+  arma::lu(lu_lower, lu_upper, permutation_matrix, A);
   for (arma::uword r = 0; r < p; ++r) {
     const arma::mat block = lambda_matrix.rows(r * p, (r + 1) * p - 1);
-    const arma::mat PB = P * block;
-    const arma::mat Y  = arma::solve(arma::trimatl(L), PB, arma::solve_opts::allow_ugly);
-    const arma::mat X  = arma::solve(arma::trimatu(U), Y,  arma::solve_opts::allow_ugly);
-    lambda_vector[r] = -0.5 * arma::trace(X);
+    const arma::mat permuted_block = permutation_matrix * block;
+    const arma::mat forward =
+      arma::solve(arma::trimatl(lu_lower), permuted_block,
+                  arma::solve_opts::allow_ugly);
+    const arma::mat solved =
+      arma::solve(arma::trimatu(lu_upper), forward,
+                  arma::solve_opts::allow_ugly);
+    lambda_vector[r] = -0.5 * arma::trace(solved);
   }
   return lambda_vector;
 }
+//==============================================================================
--- src/utils.h
+++ src/utils.h
@@ -1,48 +1,50 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <RcppArmadillo.h>
+
 Rcpp::NumericVector arma2vec(const arma::vec& x);
 arma::vec vec2arma(const Rcpp::NumericVector& x);
 arma::mat subset_matrix(const arma::mat& x, const arma::vec& y);
-arma::mat kappa_matrix(int dimension);
+arma::mat kappa_matrix(arma::uword dimension);
 arma::mat kronecker_left_identity_kappa(const arma::mat& x);
 arma::mat kronecker_identity_right_kappa(const arma::mat& x);
 arma::mat kappa_right(const arma::mat& x);
 arma::mat kronecker_sum_same(const arma::mat& x);
 arma::mat kronecker_vector_identity(const arma::vec& x);
-arma::mat kronecker_vector_matrix(const arma::vec& x,const arma::mat& Y);
+arma::mat kronecker_vector_matrix(const arma::vec& x, const arma::mat& y);
 arma::vec solve_chol_or_lu_vec(const arma::mat& X, const arma::vec& y);
-arma::mat solve_chol_or_lu_mat(const arma::mat& X, const arma::mat& y);
-
-// Compute lambda_vector[r] = -0.5 * trace( A^{-1} * block_r )
-// where block_r = lambda_matrix.rows(r*p, (r+1)*p - 1), using chol or LU.
+arma::mat solve_chol_or_lu_mat(const arma::mat& X, const arma::mat& Y);
 arma::vec lambda_from_blocks_chol_or_lu(const arma::mat& A,
                                         const arma::mat& lambda_matrix);
-
 inline void kron_self_matrix_into(arma::mat& kron_d_matrix_d_matrix_i,
                                   const arma::mat& d_matrix_i) {
   const arma::uword m = d_matrix_i.n_rows;
   const arma::uword p = d_matrix_i.n_cols;
-  if (kron_d_matrix_d_matrix_i.n_rows != m * m || kron_d_matrix_d_matrix_i.n_cols != p * p) {
+
+  if (kron_d_matrix_d_matrix_i.n_rows != m * m ||
+      kron_d_matrix_d_matrix_i.n_cols != p * p) {
     kron_d_matrix_d_matrix_i.set_size(m * m, p * p);
   }
+
   for (arma::uword j = 0; j < p; ++j) {
     const double* Dj = d_matrix_i.colptr(j);
+
     for (arma::uword k = 0; k < p; ++k) {
       const double* Dk = d_matrix_i.colptr(k);
       const arma::uword col_out = j * p + k;
       double* out = kron_d_matrix_d_matrix_i.colptr(col_out);
+
       for (arma::uword r = 0; r < m; ++r) {
         const double s = Dj[r];
         double* block = out + r * m;
+
         for (arma::uword i = 0; i < m; ++i) {
           block[i] = s * Dk[i];
         }
       }
     }
   }
-};
-
-
+}
 
 #endif
